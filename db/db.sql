329820f09ea7f423d626f1b4806ec367e236442f
Yassin Leclercq <yassinleclercq@MacBook-Pro-de-Yassin.local>
2021-02-25 15:15:07 +0100

    ajout de la db

--- db/db.sql
+++ db/db.sql
@@ -3,80 +3,79 @@ CREATE DATABASE IF NOT EXISTS BooksAPI;
 
 USE BooksAPI;
 
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users 
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` 
 (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    email VARCHAR(100),
-    password VARCHAR(255),
-     createdAt datetime DEFAULT NULL,
-    updatedA datetime DEFAULT NULL
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `email` VARCHAR(100),
+    `password` VARCHAR(255),
+    `createdAt` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL
     
 );
 
-
-
-
-DROP TABLE IF EXISTS Categories;
-CREATE TABLE Categories
+DROP TABLE IF EXISTS `Categories`;
+CREATE TABLE `Categories`
 (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100),
-     createdAt datetime DEFAULT NULL,
-    updatedA datetime DEFAULT NULL
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100),
+    `createdAt` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL
    
   
 );
-DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS `Books`;
 
-CREATE TABLE Books  
+CREATE TABLE `Books` 
 (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name VARCHAR(50),
-    auteur VARCHAR(50),
-    categorieId INT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50),
+    `auteur` VARCHAR(50),
+    `categorieId` INT,
     FOREIGN KEY (categorieId) REFERENCES Categories(id),
-     createdAt datetime DEFAULT NULL,
-    updatedA datetime DEFAULT NULL
+     `createdAt` datetime DEFAULT NULL,
+    `updatedA` datetime DEFAULT NULL
 
 );
-DROP TABLE IF EXISTS Genres;
+DROP TABLE IF EXISTS `Genres`;
 
-CREATE TABLE Genres 
+CREATE TABLE `Genres` 
 (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name VARCHAR(100),
-     createdAt datetime DEFAULT NULL,
-    updatedA datetime DEFAULT NULL
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100),
+    `createdAt` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL
     
   
 );
-DROP TABLE IF EXISTS BooksGenres;
+DROP TABLE IF EXISTS `BooksGenres`;
 
-CREATE TABLE BooksGenres  
+CREATE TABLE `BooksGenres`  
 ( 
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    bookId INT,
-    genreId INT, 
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `bookId` INT,
+    `genreId` INT, 
     FOREIGN KEY (bookId) REFERENCES Books(id),
     FOREIGN KEY (genreId) REFERENCES Genres(id),
-    createdAt datetime DEFAULT NULL,
-    updatedA datetime DEFAULT NULL
+    `createdAt` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL
   
 );
-DROP TABLE IF EXISTS Bookings;
+DROP TABLE IF EXISTS `Bookings`;
 
 
-CREATE TABLE Bookings  
+CREATE TABLE `Bookings`  
 (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    userId INT,
-    bookId INT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `userId` INT,
+    `bookId` INT,
     FOREIGN KEY (userId) REFERENCES Users(id),
     FOREIGN KEY (bookId) REFERENCES Books(id),
-    createdAt datetime DEFAULT NULL,
-    updatedA datetime DEFAULT NULL
+    `createdAt` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL
 );
 
-INSERT INTO Users (`id`, `email`, `password`) VALUES 
-(1,'Gwen@gmail.fr','azerty'),(2,'Yassin@gmail.fr','azerty'),(3,'Lorris@gmail.fr','azerty')
+
+
+INSERT INTO `Users` VALUES 
+(1,'Gwen@gmail.fr','azerty',NULL,NULL),(2,'Yassin@gmail.fr','azerty',NULL,NULL),(3,'Lorris@gmail.fr','azerty',NULL,NULL);
