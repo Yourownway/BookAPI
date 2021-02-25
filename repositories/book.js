bf5cd643f51db03d7e14124c96e1faa91e62f066
Chuck <lorris.dhedin@hotmail.fr>
2021-02-25 22:14:07 +0100

    database yassin

--- repositories/book.js
+++ repositories/book.js
@@ -2,23 +2,15 @@ module.exports = (models) => {
   const book_repository = {
     getAll: async (data) => {
       return models.book.query(
-        "SELECT Books.auteur, Books.bookName, GROUP_CONCAT(Genres.genreName SEPARATOR '-') as genre ,Categories.categorieName FROM (Books INNER JOIN BooksGenres ON Books.id = BooksGenres.bookId) INNER JOIN Genres ON BooksGenres.genreId = Genres.id INNER JOIN Categories ON Books.categorieId = Categories.id  GROUP BY BooksGenres.bookId"
+        "SELECT Books.auteur, Books.name , GROUP_CONCAT(Genres.name SEPARATOR '-') as genre ,Categories.name as categorie FROM (Books INNER JOIN BooksGenres ON Books.id = BooksGenres.bookId) INNER JOIN Genres ON BooksGenres.genreId = Genres.id INNER JOIN Categories ON Books.categorieId = Categories.id  GROUP BY BooksGenres.bookId"
       );
     },
     getBySearch: async (input) => {
-      console.log(input);
       return models.book.query(
-        `SELECT DISTINCT Books.auteur, Books.bookName FROM (Books INNER JOIN BooksGenres ON Books.id = BooksGenres.bookId) INNER JOIN Genres ON BooksGenres.genreId = Genres.id INNER JOIN Categories ON Categories.id = Books.id WHERE Genres.genreName LIKE '%${input}%' OR Categories.categorieName LIKE '%${input}%'OR Books.bookName LIKE '%${input}%' OR Books.auteur LIKE '%${input}%' `
+        `SELECT DISTINCT Books.auteur, Books.name as bookName FROM (Books INNER JOIN BooksGenres ON Books.id = BooksGenres.bookId) INNER JOIN Genres ON BooksGenres.genreId = Genres.id INNER JOIN Categories ON Categories.id = Books.id WHERE Genres.name LIKE '%${input}%' OR Categories.name LIKE '%${input}%'OR Books.name LIKE '%${input}%' OR Books.auteur LIKE '%${input}%' `
       );
     },
   };
 
   return book_repository;
 };
-
-// `SELECT Books.id, Categories.categorieName FROM Categories INNER JOIN Books ON Books.categorieId = Categories.id`
-// "SELECT Books.id ,Books.auteur, Books.bookName, Categories.categorieName FROM (Books INNER JOIN Categories ON Books.categorieId = Categories.id) "
-// "SELECT Books.auteur, Books.bookName, Categories.categorieName FROM Books INNER JOIN Categories ON Books.categorieId = Categories.id"
-
-// "SELECT * FROM Genres INNER JOIN Books ON Genres.bookId=Books.id "
-// "SELECT Genres.genreName, Books.bookName FROM (Genres INNER JOIN BooksGenres ON Genres.id = BooksGenres.genreId) INNER JOIN Books ON Books.id = BooksGenres.bookId"
--- repositories/booking.js
+++ repositories/booking.js
@@ -2,7 +2,12 @@ module.exports = (models) => {
   const booking_repository = {
     rentBook: async (data) => {
       return models.booking.query(
-        `INSERT INTO Bookings (userId, bookId)  VALUES ( ${data.userId}, ${data.bookId}) WHERE Bookings.bookId NOT IN 2`
+        // `INSERT INTO Bookings (userId, bookId)  VALUES ( ${data.userId}, ${data.bookId}) WHERE Bookings.bookId NOT IN 2`
+        `INSERT INTO Bookings (userId, bookId)
+        SELECT * FROM (SELECT ${data.userId}, ${data.bookId}) AS tmp
+        WHERE NOT EXISTS (
+            SELECT bookId FROM Bookings WHERE bookId = ${data.bookId}
+        ) LIMIT 1;`
       );
     },
   };
